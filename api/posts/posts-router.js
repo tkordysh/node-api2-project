301f7605a3f271c0e812f41a368944ec32071015
Tony Kordysh <tkordysh@gmail.com>
2022-05-10 17:10:40 -0500

    first get functionality in place

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,7 +4,18 @@ const Post = require('./posts-model')
 const router = express.Router();
 
 
-//GET 
+//GET all
+router.get('/', (req, res) => {
+    Post.find(req.query)
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ message: "The posts information could not be retrieved" })
+    })
+})
+
 
 
 module.exports = router;
