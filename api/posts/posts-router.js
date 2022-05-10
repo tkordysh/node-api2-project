66f75e3bf6c9d9f81129bb014d1d32ee5c70a390
Tony Kordysh <tkordysh@gmail.com>
2022-05-10 17:16:17 -0500

    get by id functionality added

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -16,6 +16,22 @@ router.get('/', (req, res) => {
     })
 })
 
+//GET by id
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    Post.findById(id)
+    .then(post => {
+        if(post) {
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist" })
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ message: "The post information could not be retrieved" })
+    })
+})
 
 
 module.exports = router;
