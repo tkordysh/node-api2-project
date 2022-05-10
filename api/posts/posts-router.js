479a3863740503ff2f082663bd44637c454a3e68
Tony Kordysh <tkordysh@gmail.com>
2022-05-10 17:05:04 -0500

    added router set up

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,10 @@
 // implement your posts router here
+const express = require('express');
+const Post = require('./posts-model')
+const router = express.Router();
+
+
+//GET 
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,12 +1,22 @@
 // implement your server here
 // require your posts router and connect it here
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
 server.use(express.json());
 
+const postsRouter = require("./posts/posts-router");
+
+server.use("/api/posts", postsRouter);
 
 
+
+server.get('/', (req, res) => {
+    res.send(`
+      <h2>Blog API</h>
+      <p>Welcome to the Blog API</p>
+    `);
+  });
 
 module.exports = server;
